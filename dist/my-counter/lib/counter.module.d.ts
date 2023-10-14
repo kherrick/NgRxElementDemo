796ab35d237d9db21a743f5f01f47d953be5bccb
Karl Herrick <me@karlherrick.com>
2023-10-14 16:09:01 -0500

    npx @angular/cli@latest new ngrxElementDemo --create-application=false --skip-git

--- dist/my-counter/lib/counter.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { Injector } from '@angular/core';
-export declare class CounterModule {
-    private injector;
-    constructor(injector: Injector);
-}
--- dist/my-counter/lib/counter.reducer.d.ts
+++
@@ -1,3 +0,0 @@
-import { Action } from '@ngrx/store';
-export declare const initialState = 0;
-export declare function counterReducer(state: number, action: Action): number;
